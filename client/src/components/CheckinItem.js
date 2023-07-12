1c799564a716b12ac102e5bec4652fc78142f4ee
Steven <stevenjohnmorgan@gmail.com>
2023-07-12 15:28:17 +0100

    updated checkin status display:

--- client/src/components/CheckinItem.js
+++ client/src/components/CheckinItem.js
@@ -3,7 +3,7 @@ const CheckinItem = ({guest, deleteCheckin}) => {
     <>
     <h1>Name : {guest.name}</h1>
     <h2>Email : {guest.email}</h2>
-    <p>status : {guest.status}</p>
+    <p>status : </p>{guest.status ? <p>Checked in</p> : <p>Checked out</p> }
     <button onClick={() => deleteCheckin(guest._id)}> 🗑 </button>
     </>
   )
