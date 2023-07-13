0b6c8bc145b500d213ab911d35d0bca4681ae9fd
Paul Dimitris Kyriou <pdkyriou@gmail.com>
2023-07-13 08:16:46 +0100

    fixed checkInOut

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,50 @@
-import "./App.css";
 import React, { useState, useEffect } from 'react';
-import CheckinForm from "./components/CheckinForm";
+import CheckInOut from "./components/CheckInOut";
 import CheckinList from "./components/CheckinList";
-import { getCheckins, postCheckin, deleteCheckin } from "./components/CheckinService"; 
-
+import { getCheckins, postCheckin, deleteCheckin } from "./components/CheckinService";
 
 function App() {
   const [checkinList, setCheckinList] = useState([]);
 
-  useEffect(()=>{
-    getCheckins()
-    .then((data)=>{
-      console.log(data);
-      setCheckinList(data)
-    })
-  },[]);
+  useEffect(() => {
+    fetchCheckins();
+  }, []);
 
-  const handleCheckin = (checkin) => {
-    const tempCheckins = [...checkinList]
-    tempCheckins.push(checkin);
-    setCheckinList(tempCheckins)
-  }
+  const fetchCheckins = async () => {
+    try {
+      const data = await getCheckins();
+      setCheckinList(data);
+    } catch (error) {
+      console.error("Error fetching check-ins:", error);
+    }
+  };
+
+  // Label: Changed function name to "handleCheckInOut"
+  const handleCheckInOut = async (checkin, operationType) => {
+    try {
+      if (operationType === "checkin") {
+        const newCheckin = await postCheckin(checkin);
+        // Label: Spread previous checkinList and add newCheckin
+        setCheckinList([...checkinList, newCheckin]);
+      } else if (operationType === "checkout") {
+        await deleteCheckin(checkin);
+        // Label: Filter out the checkin being checked out
+        setCheckinList(checkinList.filter((item) => item._id !== checkin));
+      }
+    } catch (error) {
+      console.error("Error handling check-in/out:", error);
+    }
+  };
 
   return (
     <>
-      <h1>Hotel Checkin</h1>
-      <CheckinForm handleCheckin={handleCheckin}  />
-      <CheckinList guests={checkinList} deleteCheckin={deleteCheckin} />
+      <h1>Hotel Check-in</h1>
+      {/* Label: Pass "handleCheckInOut" as prop */}
+      <CheckInOut addCheckInOut={handleCheckInOut} />
+      {/* Label: Pass "deleteCheckin" as prop */}
+      <CheckinList guests={checkinList} deleteCheckin={handleCheckInOut} />
     </>
   );
 }
-
-
 
 export default App;
--- client/src/components/CheckInOut.js
+++ client/src/components/CheckInOut.js
@@ -2,18 +2,23 @@ import React, { useState } from "react";
 
 const CheckInOut = ({ addCheckInOut }) => {
   const [formData, setFormData] = useState({});
-  const [isCheckedIn, setIsCheckedIn] = useState(false);
+  const [checkedInPeople, setCheckedInPeople] = useState([]);
 
   const handleCheckIn = (event) => {
     event.preventDefault();
-    setIsCheckedIn(true);
-    addCheckInOut(formData);
+    const newPerson = {
+      id: Date.now(), // Generate a unique ID for each person
+      ...formData
+    };
+    setCheckedInPeople([...checkedInPeople, newPerson]); // Add the new person to the checked-in list
+    setFormData({});
+    addCheckInOut(newPerson);
   };
 
-  const handleCheckOut = (event) => {
-    event.preventDefault();
-    setIsCheckedIn(false);
-    addCheckInOut(formData);
+  const handleCheckOut = (personId) => {
+    const updatedPeople = checkedInPeople.filter(person => person.id !== personId); // Remove the person from the checked-in list
+    setCheckedInPeople(updatedPeople);
+    addCheckInOut(personId); // Pass the person ID to the addCheckInOut function
   };
 
   const handleChange = (event) => {
@@ -21,27 +26,39 @@ const CheckInOut = ({ addCheckInOut }) => {
   };
 
   return (
-    <form onSubmit={isCheckedIn ? handleCheckOut : handleCheckIn}>
-      <label htmlFor="name">Name:</label>
-      <input
-        type="text"
-        id="name"
-        value={formData.name || ""}
-        onChange={handleChange}
-        required
-      />
+    <div>
+      <form onSubmit={handleCheckIn}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={formData.name || ""}
+          onChange={handleChange}
+          required
+        />
 
-      <label htmlFor="roomNumber">Room Number:</label>
-      <input
-        type="number"
-        id="roomNumber"
-        value={formData.roomNumber || ""}
-        onChange={handleChange}
-        required
-      />
+        <label htmlFor="emailAddress">Email Address:</label>
+        <input
+          type="email"
+          id="emailAddress"
+          value={formData.emailAddress || ""}
+          onChange={handleChange}
+          required
+        />
 
-      <input type="submit" value={isCheckedIn ? "Check Out" : "Check In"} />
-    </form>
+        <input type="submit" value="Check In" />
+      </form>
+
+      <h2>Checked-in People:</h2>
+      <ul>
+        {checkedInPeople.map(person => (
+          <li key={person.id}>
+            {person.name} ({person.emailAddress}) 
+            <button onClick={() => handleCheckOut(person.id)}>Check Out</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
